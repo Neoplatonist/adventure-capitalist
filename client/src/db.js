cf4f7ef9c9d031ef438d479717b0a7c279e667a8
Neoplatonist <mr.neoplatonist@gmail.com>
2020-04-23 01:27:12 +0900

    added industry database

---
+++ client/src/db.js
@@ -0,0 +1,102 @@
+export default [
+    {
+        name: 'Farmland',
+        income: 1,
+        baseCost: 0,
+        currentCost: 0,
+        upgrade: 0,
+        numberOwned: 0,
+        manager: false,
+        wait: 1000
+    },
+    {
+        name: 'Mineral Extractor',
+        income: 1,
+        baseCost: 0,
+        currentCost: 0,
+        upgrade: 0,
+        numberOwned: 0,
+        manager: false,
+        wait: 3000
+    },
+    {
+        name: 'Particle Generator',
+        income: 1,
+        baseCost: 0,
+        currentCost: 0,
+        upgrade: 0,
+        numberOwned: 0,
+        manager: false,
+        wait: 6000
+    },
+    {
+        name: 'Trading Post',
+        income: 1,
+        baseCost: 0,
+        currentCost: 0,
+        upgrade: 0,
+        numberOwned: 0,
+        manager: false,
+        wait: 12000
+    },
+    {
+        name: 'Academy',
+        income: 1,
+        baseCost: 0,
+        currentCost: 0,
+        upgrade: 0,
+        numberOwned: 0,
+        manager: false,
+        wait: 24000
+    },
+    {
+        name: 'Data Silo',
+        income: 1,
+        baseCost: 0,
+        currentCost: 0,
+        upgrade: 0,
+        numberOwned: 0,
+        manager: false,
+        wait: 48000
+    },
+    {
+        name: 'Labratory',
+        income: 1,
+        baseCost: 0,
+        currentCost: 0,
+        upgrade: 0,
+        numberOwned: 0,
+        manager: false,
+        wait: 96000
+    },
+    {
+        name: 'Colony',
+        income: 1,
+        baseCost: 0,
+        currentCost: 0,
+        upgrade: 0,
+        numberOwned: 0,
+        manager: false,
+        wait: 192000
+    },
+    {
+        name: 'Federation',
+        income: 1,
+        baseCost: 0,
+        currentCost: 0,
+        upgrade: 0,
+        numberOwned: 0,
+        manager: false,
+        wait: 384000
+    },
+    {
+        name: 'Galactic Expedition',
+        income: 1,
+        baseCost: 0,
+        currentCost: 0,
+        upgrade: 0,
+        numberOwned: 0,
+        manager: false,
+        wait: 768000
+    }
+]

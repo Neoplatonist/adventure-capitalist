d39a8ad8039c5015cea1fb9fc44c81bbfc9bfa6b
Neoplatonist <mr.neoplatonist@gmail.com>
2020-04-23 03:01:02 +0900

    added industry locked until purchased to industry db

--- client/src/db.js
+++ client/src/db.js
@@ -5,98 +5,108 @@ export default [
         baseCost: 0,
         currentCost: 0,
         upgrade: 0,
-        numberOwned: 0,
+        numberOwned: 1,
         manager: false,
-        wait: 1000
+        wait: 1000,
+        unlocked: true
     },
     {
         name: 'Mineral Extractor',
-        income: 1,
+        income: 3,
         baseCost: 0,
         currentCost: 0,
         upgrade: 0,
-        numberOwned: 0,
+        numberOwned: 1,
         manager: false,
-        wait: 3000
+        wait: 3000,
+        unlocked: false
     },
     {
         name: 'Particle Generator',
-        income: 1,
+        income: 5,
         baseCost: 0,
         currentCost: 0,
         upgrade: 0,
-        numberOwned: 0,
+        numberOwned: 1,
         manager: false,
-        wait: 6000
+        wait: 6000,
+        unlocked: false
     },
     {
         name: 'Trading Post',
-        income: 1,
+        income: 7,
         baseCost: 0,
         currentCost: 0,
         upgrade: 0,
-        numberOwned: 0,
+        numberOwned: 1,
         manager: false,
-        wait: 12000
+        wait: 12000,
+        unlocked: false
     },
     {
         name: 'Academy',
-        income: 1,
+        income: 9,
         baseCost: 0,
         currentCost: 0,
         upgrade: 0,
-        numberOwned: 0,
+        numberOwned: 1,
         manager: false,
-        wait: 24000
+        wait: 24000,
+        unlocked: false
     },
     {
         name: 'Data Silo',
-        income: 1,
+        income: 11,
         baseCost: 0,
         currentCost: 0,
         upgrade: 0,
-        numberOwned: 0,
+        numberOwned: 1,
         manager: false,
-        wait: 48000
+        wait: 48000,
+        unlocked: false
     },
     {
         name: 'Labratory',
-        income: 1,
+        income: 13,
         baseCost: 0,
         currentCost: 0,
         upgrade: 0,
-        numberOwned: 0,
+        numberOwned: 1,
         manager: false,
-        wait: 96000
+        wait: 96000,
+        unlocked: false
     },
     {
         name: 'Colony',
-        income: 1,
+        income: 17,
         baseCost: 0,
         currentCost: 0,
         upgrade: 0,
-        numberOwned: 0,
+        numberOwned: 1,
         manager: false,
-        wait: 192000
+        wait: 192000,
+        unlocked: false
     },
     {
         name: 'Federation',
-        income: 1,
+        income: 19,
         baseCost: 0,
         currentCost: 0,
         upgrade: 0,
-        numberOwned: 0,
+        numberOwned: 1,
         manager: false,
-        wait: 384000
+        wait: 384000,
+        unlocked: false
     },
     {
         name: 'Galactic Expedition',
-        income: 1,
+        income: 23,
         baseCost: 0,
         currentCost: 0,
         upgrade: 0,
-        numberOwned: 0,
+        numberOwned: 1,
         manager: false,
-        wait: 768000
+        wait: 768000,
+        unlocked: false
     }
 ]
